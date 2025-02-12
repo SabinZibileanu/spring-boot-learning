6ccb5a403e2fecfef259aca88960ad8a1a6d9d39
SabinZibileanu <s_zibileanu@yahoo.com>
2025-02-12 12:04:45 +0200

    updated the player controller

--- first-spring-project/src/main/java/org/example/firstspringproject/PlayerController.java
+++ first-spring-project/src/main/java/org/example/firstspringproject/PlayerController.java
@@ -1,20 +1,30 @@
 package org.example.firstspringproject;
 import org.example.firstspringproject.service.CSVReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 
 public class PlayerController {
     @Autowired
     private CSVReaderService csvReaderService;
+    private final static String FILE_PLAYERS_DATA = "utils/superliga_data.csv";
 
     @GetMapping("/players")
     public List<Player> getAllPlayers() {
-        String filePath = "utils/superliga_data.csv";
-        return csvReaderService.readCSV(filePath);
+
+        return csvReaderService.readCSV(FILE_PLAYERS_DATA);
+    }
+
+    @GetMapping("/risingstars")
+    public List<Player> getRisingStars() {
+
+        List<Player> all_players = csvReaderService.readCSV(FILE_PLAYERS_DATA);
+
+        return all_players.stream()
+                .filter(player -> player.getAge() <= 24)
+                .collect(Collectors.toList());
     }
 }
